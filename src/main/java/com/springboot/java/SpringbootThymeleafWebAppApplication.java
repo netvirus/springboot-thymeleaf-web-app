0f4b9fb85522aef18c06fbec80fb2e82ebe1aae8
netvirus <yariknetvirus@gmail.com>
2020-07-30 15:16:50 +0700

    * Added war file support and fixed some errors

--- src/main/java/com/springboot/java/SpringbootThymeleafWebAppApplication.java
+++ src/main/java/com/springboot/java/SpringbootThymeleafWebAppApplication.java
@@ -2,9 +2,10 @@ package com.springboot.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootThymeleafWebAppApplication {
+public class SpringbootThymeleafWebAppApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootThymeleafWebAppApplication.class, args);
